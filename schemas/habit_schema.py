8c19796b9f54a54a2c8946986cd21a6f760490fb
KEgubov <kirilegubov@gmail.com>
2026-01-07 18:58:03 +0200

    feat: Added input validation scheme for daily and weekly habits in habit_schema.py

--- schemas/habit_schema.py
+++ schemas/habit_schema.py
@@ -16,3 +16,14 @@ class GoalDaysHabit(Enum):
     ONE_MONTH = "1 month"
     SIX_MONTHS = "6 months"
     ONE_YEAR = "1 year"
+
+class DailyHabitSchema(BaseModel):
+    habit_id: Annotated[int, Field(ge=0)]
+    streak: Annotated[int, Field(ge=0)]
+    habit_name: Annotated[str, Field(min_length=1, max_length=500)]
+    habit_description: Annotated[str, Field(min_length=1, max_length=500)]
+    category: Annotated[CategoryHabit, Field(min_length=1, max_length=500)]
+    goal_days: Annotated[GoalDaysHabit, Field(min_length=1, max_length=500)]
+
+class WeeklyHabitSchema(DailyHabitSchema):
+    weekly_streak: Annotated[int, Field(ge=0)]
