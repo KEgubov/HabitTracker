bf85e4dd5d4be00669a39c671db2d5566df88bce
KEgubov <kirilegubov@gmail.com>
2026-01-10 19:27:00 +0200

    ref: Type annotations have been added to Enum classes to serialize into JSON

--- schemas/habit_schema.py
+++ schemas/habit_schema.py
@@ -2,18 +2,18 @@ from pydantic import BaseModel, Field
 from typing import Annotated
 from enum import Enum
 
-class TypeHabit(Enum):
+class TypeHabit(str, Enum):
     DAILY = "daily"
     WEEKLY = "weekly"
 
-class CategoryHabit(Enum):
+class CategoryHabit(str, Enum):
     HEALTH = "health"
     PRODUCTIVITY = "productivity"
     SPORTS = "sports"
     SELF_DEVELOPMENT = "self development"
     FINANCE = "finance"
 
-class GoalDaysHabit(Enum):
+class GoalDaysHabit(int, Enum):
     ONE_DAY = 1
     ONE_WEEK = 7
     TWO_WEEK = 14
@@ -21,13 +21,13 @@ class GoalDaysHabit(Enum):
     SIX_MONTHS = 180
     ONE_YEAR = 365
 
+
 class DailyHabitSchema(BaseModel):
-    habit_id: Annotated[int, Field(ge=0)]
-    streak: Annotated[int, Field(ge=0)]
     habit_name: Annotated[str, Field(min_length=1, max_length=500)]
     habit_description: Annotated[str, Field(min_length=1, max_length=500)]
-    category: Annotated[CategoryHabit, Field(min_length=1, max_length=500)]
-    goal_days: Annotated[GoalDaysHabit, Field(min_length=1, max_length=500)]
+    category: CategoryHabit
+    type_habit: TypeHabit = TypeHabit.DAILY
 
 class WeeklyHabitSchema(DailyHabitSchema):
+    type_habit: TypeHabit = TypeHabit.WEEKLY
     weekly_streak: Annotated[int, Field(ge=0)]
