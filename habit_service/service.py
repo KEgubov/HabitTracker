48b1bb37bfe47ead05e9845b8d2c096b0a8cde16
KEgubov <kirilegubov@gmail.com>
2026-01-19 17:23:19 +0200

    eat: Added a method for displaying habits by category and improved code readability

--- habit_service/service.py
+++ habit_service/service.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-from typing import Optional
 from habit_storage.json_storage import HabitJsonStorage
 from models.base import DailyHabit
 from schemas.habit_schema import DailyHabitSchema, GoalDaysHabit, CategoryHabit
 
 
 class HabitService:
-    def __init__(self, storage: HabitJsonStorage):
+    def __init__(self, storage: HabitJsonStorage) -> None:
         self.storage = storage
         self.habits_data = storage.load()
 
@@ -44,11 +43,11 @@ class HabitService:
 
             days_since_last = (datetime.now() - last_completed_dt).days
             if days_since_last > 1:
-                habit.streak = 0
+                habit["streak"] = 0
                 return False
         return True
 
-    def _update_goal_days(self, habit) -> Optional[str]:
+    def _update_goal_days(self, habit: dict) -> str:
         goal_map = {
             1: GoalDaysHabit.ONE_WEEK,
             7: GoalDaysHabit.TWO_WEEK,
@@ -63,9 +62,9 @@ class HabitService:
                     f"Congratulations! You've reached your goal! "
                     f"New target - {goal.value} days!"
                 )
-        return f"Current streak - {habit["streak"]} days"
+        return f"Current streak - {habit['streak']} days"
 
-    def generate_id(self):
+    def generate_id(self) -> int:
         if self.habits_data:
             try:
                 max_id = max(habit.get("habit_id", 0) for habit in self.habits_data)
@@ -76,7 +75,7 @@ class HabitService:
             next_id = 1
         return next_id
 
-    def create_habit(self, habit_schema: DailyHabitSchema):
+    def create_habit(self, habit_schema: DailyHabitSchema) -> str:
         habit = DailyHabit(
             habit_id=self.generate_id(),
             habit_name=habit_schema.habit_name,
@@ -87,27 +86,27 @@ class HabitService:
         self.storage.save(self.habits_data)
         return f"Habit - '{habit_schema.habit_name.title()}' added!"
 
-    def remove_habit(self, habit_id: int):
+    def remove_habit(self, habit_id: int) -> str:
         for i, habit in enumerate(self.habits_data):
             if habit["habit_id"] == habit_id:
                 del self.habits_data[i]
                 self.storage.save(self.habits_data)
-                return f"Habit - {habit["habit_name"]} removed!"
+                return f"Habit - {habit['habit_name']} removed!"
         return "Habit not found!"
 
     def remove_all_habits(self):
         return self.storage.clear()
 
-    def complete_habit(self, habit_id: int):
+    def complete_habit(self, habit_id: int) -> str:
         for habit in self.habits_data:
             if habit["habit_id"] == habit_id:
                 habit["completed"] = True
                 message = self._increase_streak(habit_id)
                 self.storage.save(self.habits_data)
-                return f"Habit - '{habit["habit_name"]}' Completed!" f"{message}"
+                return f"Habit - '{habit['habit_name']}' Completed!" f"{message}"
         return "Habit not found!"
 
-    def show_habit(self, habit_id: int):
+    def show_habit(self, habit_id: int) -> str:
         if not self.habits_data:
             return f"Habits not found!"
         result = "Habit:\n"
@@ -122,27 +121,33 @@ class HabitService:
         return result
 
     def show_all_habits(self):
-        if not self.habits_data:
-            return f"Habits not found!"
-        result = "All Habits:\n"
+        category = {}
         for habit in self.habits_data:
-            result += (
-                f"\nID: {habit['habit_id']} | "
-                f"Name: {habit['habit_name']} | "
-                f"Category: {habit['category']} | "
-                f"Streak: {habit['streak']} days"
-            )
-        return result
+            cat = habit["category"]
+            if cat not in category:
+                category[cat] = []
+            category[cat].append(habit)
 
-    # ДОБАВИТЬ ГРУППИРОВКУ ПО ПРИВЫЧКАМ
+        result = ""
+        for cat, habits in category.items():
+            result += f"Category: {cat.title()}\n"
+            for habit in habits:
+                result += (
+                    f"  ID: {habit['habit_id']} | "
+                    f"Name: {habit['habit_name']} | "
+                    f"Streak: {habit['streak']} days\n"
+                )
+            result += "\n"
+
+        return result.rstrip()
 
 
 hs = HabitService(HabitJsonStorage())
 # print(hs.create_habit(DailyHabitSchema(habit_name="test",
 #                                        habit_description="test",
-#                                        category=CategoryHabit.HEALTH)))
-# print(hs.complete_habit(3))
+#                                        category=CategoryHabit.PRODUCTIVITY)))
+# print(hs.complete_habit(2))
 # print(hs.remove_habit(1))
 # print(hs.show_habit(1))
-# print(hs.show_all_habits())
+print(hs.show_all_habits())
 # print(hs.remove_all_habits())
