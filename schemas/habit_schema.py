0e925c838ae87f46f752ad08cac22c84a540ba09
KEgubov <kirilegubov@gmail.com>
2026-01-20 19:10:11 +0200

    feat: Added AchievementHabit class, which stores constants with achievements

--- schemas/habit_schema.py
+++ schemas/habit_schema.py
@@ -16,11 +16,20 @@ class CategoryHabit(str, Enum):
 class GoalDaysHabit(int, Enum):
     ONE_DAY = 1
     ONE_WEEK = 7
-    TWO_WEEK = 14
+    THREE_WEEKS = 21
     ONE_MONTH = 30
+    TWO_MONTHS = 60
     SIX_MONTHS = 180
     ONE_YEAR = 365
 
+class AchievementHabit(str, Enum):
+    ONE_DAY = "I started – and this is a victory!"
+    ONE_WEEK = "Willpower Week"
+    THREE_WEEKS = "21 days - the habit has taken root!"
+    ONE_MONTH = "30 days – I can do more!"
+    TWO_MONTHS = "Two months without failures is discipline!"
+    SIX_MONTHS = "180 Days of Power"
+    ONE_YEAR = "365 days – I did it!"
 
 class DailyHabitSchema(BaseModel):
     habit_name: Annotated[str, Field(min_length=1, max_length=500)]
